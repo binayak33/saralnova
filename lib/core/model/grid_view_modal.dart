084b86dab737a2771a565acb6b70a428dc6382e7
sanju <sanju@skybase.com.np>
2024-02-28 12:06:35 +0545

    Add: Grid View Modal

---
+++ lib/core/model/grid_view_modal.dart
@@ -0,0 +1,9 @@
+class Data {
+  String icon;
+  String text;
+
+  Data({
+    required this.icon,
+    required this.text,
+  });
+}
--- lib/features/screens/Home/home_screen.dart
+++ lib/features/screens/Home/home_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GridView.builder(
+        itemCount: 8,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 5, mainAxisSpacing: 5),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: double.infinity,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 137, 137, 137).withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
